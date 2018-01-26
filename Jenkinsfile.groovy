b05bfa89f8af2a031a9410f905ed143c1ec8b1bb
Raj <Raj@172.16.159.199>
2018-01-26 15:37:16 -0500

    f

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,43 @@
+import groovy.json.JsonSlurperClassic
+
+def nodeName = 'las-macbook-pro'
+def id = 'lamobilehost1'
+def attProjectPath = 'att'
+
+def FAT_builds_iOS = "att/builds/iOS"
+def FAT_builds_Android = "att/builds/Android"
+
+def name = "dfw"
+def TOKEN = "f68ff2dabda541fc86ef870ea37d8499" //# f68ff2dabda541fc86ef870ea37d8499
+
+def VERSION_NUMBER_ANDROID
+def VERSION_NUMBER_IOS
+
+def APP_ID_ANDROID
+def APP_ID_IOS
+
+def PLATFROM_ANDROID
+def PLATFROM_IOS
+
+def TEMP_FILE = "app_versions"
+def startTime
+def endTime
+
+
+node(nodeName) {
+    try {
+ 
+		stages {
+			stage('Build') {
+				//checkout scm
+				sh 'mvn clean install'
+			}
+	
+			 
+		}
+		
+		
+    } catch (all) {
+        println all
+    }
+}
