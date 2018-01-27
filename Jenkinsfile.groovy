6eb9a82753c815215485ce3678fb889117ec887f
Raj <Raj@172.16.159.199>
2018-01-27 05:39:41 -0500

    k

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,17 +13,19 @@ import groovy.json.JsonSlurperClassic
 //def VERSION_NUMBER_ANDROID
 //def VERSION_NUMBER_IOS
 
-def APP_ID_ANDROID
-def APP_ID_IOS
+//def APP_ID_ANDROID
+//def APP_ID_IOS
+//
+//def PLATFROM_ANDROID
+//def PLATFROM_IOS
+//
+//def TEMP_FILE = "app_versions"
+//def startTime
+//def endTime
 
-def PLATFROM_ANDROID
-def PLATFROM_IOS
-
-def TEMP_FILE = "app_versions"
-def startTime
-def endTime
-
-
+def username = "rajp@perfectomobile.com"
+def password = "Perfecto123"
+def cloudUrl = "ps.perfectomobile.com"
 
 node {
     try {
@@ -49,76 +51,48 @@ node {
 	    }
 	    
 	    	
-
-			stage('Installs BUilds on iOS Perfecto Devices') {
-				echo 'Installs BUilds on iOS Perfecto Devices.....'	
+		stage('Installs BUilds on iOS Perfecto Devices') {
+				echo 'Installs Builds on iOS Perfecto Devices.....'	
 			
-				def username = "rajp@perfectomobile.com"
-				def password = "Perfecto123"	
-				def cloudUrl = "ps.perfectomobile.com"
-				def	DynamicFields = "ipaVersionNumber-Raj"	
-				def iOSDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]	
-							
+				def	DynamicFields = "ipaVersionNumber-Raj"
 				def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
 				def slurper = new groovy.json.JsonSlurperClassic()
 				def startCommand = slurper.parseText(startResponse.content)
-			
 				def executionID = startCommand.executionId
-				//println "ExecutionID:  ${executionID}"
-						
+				slurper=null
+				println "$executionID}"
+				def iOSDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
+	
 				/* Device Open - Start */
-				def openResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + iOSDeviceList[1]
-				println openResponse
- 
+				try {
+					def openResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + iOSDeviceList[1]
+					println openResponse
+				} catch (all) {
+					echo 'Failed to Open Device....Catch'
+					println all
+				}
+				
 				/* Set Dynamic Field */
-		//		def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/"+${iOSDeviceList[1]}+"?operation=update&user=${username}&password=${password}&dynamicField.ipaVersion=${DynamicFields}"
-		//		println dynamicFiled
+				//def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/"+${iOSDeviceList[1]}+"?operation=update&user=${username}&password=${password}&dynamicField.ipaVersion=${DynamicFields}"
+				//println dynamicFiled
 			
 				/* Device Close - End */
-				def stopResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
-				println stopResponse
-				
+				try {
+					def stopResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
+					println stopResponse
+				} catch (all) {
+					echo 'Failed to Close Device....Catch'
+					println all
+				}
 
 				parallel(
 				    	a: {						
-//				    		def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
-//							def slurper = new groovy.json.JsonSlurperClassic()
-//							def startCommand = slurper.parseText(startResponse.content)
-//							def executionID = startCommand.executionId
-//							echo 'ExecutionID: ${executionID}'
-				    	    
-//				    	    
-//			    	    	/* Device Open - Start */
-//							def openResponse = httpRequest url: "https://dfw-directv.perfectomobile.com/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + iOSDeviceList[1]
-//							println openResponse
-//			 
-//							/* Set Dynamic Field */
-//							def dynamicFiled = httpRequest url:"https://mycloud.perfectomobile.com/services/handsets/iOSDeviceList[1]?operation=update&user=${username}&password=${password}&description=${DynamicFields}"
-//						    println dynamicFiled
-//						
-//							/* Device Close - End */
-//							def stopResponse = httpRequest url: "https://dfw-directv.perfectomobile.com/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
-//							println stopResponse
+							echo 'Branch b'
 				    	},
 						b: {
-					//		def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
-					//		def slurper = new groovy.json.JsonSlurperClassic()
-					//		def startCommand = slurper.parseText(startResponse.content)
-					//		def executionID = startCommand.executionId
-					//		echo 'ExecutionID: ${executionID}'
+				
 				    	    echo 'Branch b'
-				    	    
-				    	    /* Device Open - Start */
-					//		def openResponse = httpRequest url: "https://dfw-directv.perfectomobile.com/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + iOSDeviceList[2]
-					//		println openResponse
-			
-							/* Set Dynamic Field */
-					//		def dynamicFiled = httpRequest url:"https://mycloud.perfectomobile.com/services/handsets/iOSDeviceList[1]?operation=update&user=${username}&password=${password}&description=${DynamicFields2}"
-					//	    println dynamicFiled
-						    
-							/* Device Close - End */
-					//		def stopResponse = httpRequest url: "https://dfw-directv.perfectomobile.com/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
-					//		println stopResponse
+				  
 				    	}
 				    
 				    )
@@ -127,12 +101,7 @@ node {
 
 			}
 			
-			
-			
-			
-			
-			
-			
+		
 			stage('Pre-Production') {
 				echo 'Pre-Production.....'
 			}
@@ -141,10 +110,7 @@ node {
 				echo 'Production.....'
 			}
 			
-		parallel (
-		
-		
-		)
+
 		
 		
     } catch (all) {
