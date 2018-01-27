9658d00bf9630f2641cc821dd862cfb58c65766d
Raj <Raj@172.16.159.199>
2018-01-27 04:42:27 -0500

    ii

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -56,7 +56,7 @@ node {
 				def username = "rajp@perfectomobile.com"
 				def password = "Perfecto123"	
 				def cloudUrl = "ps.perfectomobile.com"
-				def	DynamicFields = "Version=ipa-1234-Raj"	
+				def	DynamicFields = "ipaVersionNumber-Raj"	
 				def iOSDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]	
 							
 				def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
@@ -71,7 +71,7 @@ node {
 				println openResponse
  
 				/* Set Dynamic Field */
-				def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/" + iOSDeviceList[1] + "?operation=update&user=${username}&password=${password}&description=${DynamicFields}"
+				def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/"+iOSDeviceList[1]+"?operation=update&user=${username}&password=${password}&dynamicField.ipaVersion=${DynamicFields}"
 				println dynamicFiled
 			
 				/* Device Close - End */
