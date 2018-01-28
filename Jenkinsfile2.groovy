0d8251ab73c5072cb5e00e11d3ed059ca28cdd74
Raj <Raj@172.16.159.199>
2018-01-27 19:47:02 -0500

    j

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -31,7 +31,6 @@ def username = "rajp@perfectomobile.com"
 def password = "Perfecto123"
 def cloudUrl = "ps.perfectomobile.com"
 def	DynamicFields = "Test-Version-Raj.ipa"
-def iOSDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
 
 node {
     try {
@@ -59,7 +58,7 @@ node {
 		stage('Installs iOS Builds on Perfecto Devices') {
 				echo 'Installs Builds on iOS Perfecto Devices.....'		
 					
-				iOSLoad(iosDeviceName)
+				iOSLoad()
 				
 			
 			}
@@ -67,7 +66,7 @@ node {
 			stage('Installs Android Builds on Perfecto Devices') {
 				echo 'Installs Builds on Android Perfecto Devices.....'
 					
-				androidLoad(iosDeviceName)
+				androidLoad()
 			}
 		
 		
@@ -79,7 +78,9 @@ node {
 
 }
 
-def iOSLoad(iosDeviceName) {
+def iOSLoad() {
+	def iOSDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
+	
 	parallel (
 		deviceA: {
 			echo 'Building AProject.....'
@@ -102,7 +103,10 @@ def iOSLoad(iosDeviceName) {
 	)
 }
 
-def androidLoad(iosDeviceName) {
+def androidLoad() {
+	def androidDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
+	
+	
 	parallel (
 		deviceA: {
 			echo 'Building AProject.....'
