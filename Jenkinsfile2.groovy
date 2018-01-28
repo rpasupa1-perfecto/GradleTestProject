d6485b32482364c83370e7467975f62669dcd751
Raj <Raj@172.16.159.199>
2018-01-27 21:58:08 -0500

    kkkkk

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -90,33 +90,10 @@ def iOSLoad() {
 		"0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
 	
 	parallel (
-		deviceA: {
+		def deviceName = iOSDeviceList[0]
+		deviceName: {	
 			echo 'Building AProject.....'
-			deviceStart: ('Device-Start') {
-				checkout scm
-				echo 'Checking Code out.....'
-			}
-			
-//			stage('Open-Device') {
-//				checkout scm
-//				echo 'Checking Code out.....'
-//			}
-//			
-//			stage('SetDynamicField') {
-//				checkout scm
-//				echo 'Checking Code out.....'
-//			}
-//			
-//			stage('Close-Device') {
-//				checkout scm
-//				echo 'Checking Code out.....'
-//			}
-//			
-//			stage('Device-End') {
-//				checkout scm
-//				echo 'Checking Code out.....'
-//			}
-			
+			iosAndroidInstall(iOSDeviceList[0])
 		},
 		deviceB: {
 			echo 'Building B Project.....'
@@ -139,7 +116,6 @@ def iOSLoad() {
 def androidLoad() {
 	def androidDeviceList = ["41EEF156EA10EDAB41632651F7AD2A4C4CB502ED","1C3B401545D2CDBEC9D323460D914AD7319F31D9","3133BB296C46FA2250362A227BA462A56ED11A45","DD992AFA0B69A5E2C2006A7A657690476B0086FE","C37BAE1934AE7DD0AE3355F77146C7A65579CAA3","0C2210C8EBD9A1FB421A8D0A692E6C72F85E4C9E"]
 	
-	
 	parallel (
 		deviceA: {
 			echo 'Building AProject.....'
@@ -160,4 +136,53 @@ def androidLoad() {
 			echo 'Building F Project.....'
 		}
 	)
+}
+
+
+def iosAndroidInstall(deviceList) {
+	
+	/* Device Start */
+	def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
+	def slurper = new groovy.json.JsonSlurperClassic()
+	def startCommand = slurper.parseText(startResponse.content)
+	def executionID = startCommand.executionId
+	slurper=null
+	println "$executionID}"
+
+	/* Open Device Connection */
+	try {
+		def openResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + deviceList
+		println openResponse
+	} catch (all) {
+		echo 'Failed to Open Device....Catch'
+		println all
+	}
+	
+	/* Set Dynamic Field */
+	try {
+		def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/"+deviceList+"?operation=update&user=${username}&password=${password}&dynamicField.ipa=${DynamicFields}"
+		println dynamicFiled
+	} catch (all) {
+		echo 'Failed to Set Dynamic Field....Catch'
+		println all
+	}
+
+	/* Close Device */
+	try {
+		def closeResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=close&param.deviceId=" + deviceList
+		println closeResponse
+	} catch (all) {
+		echo 'Failed to Close Device....Catch'
+		println all
+	}
+		
+	/* Destroy Device Object */
+	try {
+		def stopResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
+		println stopResponse
+	} catch (all) {
+		echo 'Failed to QUIT Device....Catch'
+		println all
+	}
+	
 }
