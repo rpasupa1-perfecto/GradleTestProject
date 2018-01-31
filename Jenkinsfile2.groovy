098e04898f9563508b29c20bc1b4ebe5a2e6cb72
Raj <Raj@172.16.159.221>
2018-01-31 14:29:19 -0500

    h

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -140,37 +140,34 @@ def androidInstall(deviceList) {
 	def username = "rajp@perfectomobile.com"
 	def password = "Perfecto123"
 	def cloudUrl = "ps.perfectomobile.com"
-	def	DynamicFields = "Test-Version-Raj.ipa"
+	def	DynamicFields = "Test-Android-Version-Raj.apk"
 	def appID = "com.att.mobile.dfw"
 	def appLocation = "Raj/dfw-android-1001001108.apk"
 	
 	/* Device Start */
+	println "Start Connection with Perfecto"
 	def startResponse = httpRequest url: "https://${cloudUrl}/services/executions?operation=start&user=${username}&password=${password}"
-	def slurper = new groovy.json.JsonSlurperClassic()
-	def startCommand = slurper.parseText(startResponse.content)
-	def executionID = startCommand.executionId
-	slurper=null
-	println "$executionID}"
+	def executionID = getExecutionID(startResponse)
 
-		
 	/* Make device Reservation */
 	
 	
 	
 	/* Open Device Connection */
 	try {
+		println "Start Device Connection with Perfecto"
 		def openResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=open&param.deviceId=" + deviceList
-		println openResponse.content
+		printResponse(openResponse)
 	} catch (all) {
 		echo 'Failed to Open Device....Catch'
 		println all
-	} 
+	}
 	
 	/* Set Dynamic Field */
 	try {
-		println "Setting Dynamic Field ipa/apk File Name for device:  " + deviceList
+		println "Setting Dynamic Field for device:  " + deviceList
 		def dynamicFiled = httpRequest url:"https://${cloudUrl}/services/handsets/"+deviceList+"?operation=update&user=${username}&password=${password}&dynamicField.ipa=${DynamicFields}"
-		println dynamicFiled.content
+		printResponse(dynamicFiled)
 	} catch (all) {
 		echo 'Failed to Set Dynamic Field....Catch'
 		println all
@@ -178,9 +175,9 @@ def androidInstall(deviceList) {
 	
 	/* Uninstall Application */
 	try {
-	//	println "Uninstalling App for device:  " + deviceList
-	//	def uninstallApp = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=application&subcommand=uninstall&param.deviceId=" + deviceList + "&param.identifier=${appID}"
-	//	println uninstallApp
+		println "Uninstalling App for device:  " + deviceList
+		def uninstallApp = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=application&subcommand=uninstall&param.deviceId=" + deviceList + "&param.identifier=${appID}"
+		printResponse(uninstallApp)
 	} catch (all) {
 		echo 'Failed to Uninstall Application....Catch'
 		println all
@@ -188,13 +185,14 @@ def androidInstall(deviceList) {
 		
 	/* Reboot Phone */
 	try {
-		println "Rebooting Device:  " + deviceList
-		def rebootResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=reboot&param.deviceId=" + deviceList	
-		println rebootResponse
+//		println "Rebooting Device:  " + deviceList
+//		def rebootResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=reboot&param.deviceId=" + deviceList
+//		printResponse(rebootResponse)
 	} catch (all) {
 		echo 'Failed to Reboot Phone....Catch'
 		println all
 	}
+	
 	
 	/* Install Application */
 	try {
@@ -208,8 +206,9 @@ def androidInstall(deviceList) {
 	
 	/* Close Device */
 	try {
+		println "Close Device with Perfecto "
 		def closeResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=command&user=${username}&password=${password}&command=device&subcommand=close&param.deviceId=" + deviceList
-		println closeResponse
+		printResponse(closeResponse)
 	} catch (all) {
 		echo 'Failed to Close Device....Catch'
 		println all
@@ -217,12 +216,14 @@ def androidInstall(deviceList) {
 		
 	/* Destroy Device Object */
 	try {
+		println "End Device Driver with Perfecto "
 		def stopResponse = httpRequest url: "https://${cloudUrl}/services/executions/${executionID}?operation=end&user=${username}&password=${password}"
-		println stopResponse
+		printResponse(stopResponse)
 	} catch (all) {
 		echo 'Failed to QUIT Device....Catch'
 		println all
 	}
+	
 	
 	
 	/* Delete Reservation */
@@ -234,7 +235,7 @@ def iosInstall(deviceList) {
 	def username = "rajp@perfectomobile.com"
 	def password = "Perfecto123"
 	def cloudUrl = "ps.perfectomobile.com"
-	def	DynamicFields = "Test-Version-Raj.ipa"
+	def	DynamicFields = "Test-iOS-Version-Raj.ipa"
 	def appID = "com.att.mobile.dfw"
 	def appLocation = "Raj/dfw-1.0.5647_ios.ipa"
 	
