ee419c9b195c163cba4a112e36f67edca530f67f
Raj <Raj@172.16.159.221>
2018-04-19 08:48:04 -0500

    j

--- Jenkinsfile3.groovy
+++ Jenkinsfile3.groovy
@@ -375,10 +375,10 @@ def reportiumStepStart(executionID, stepStartName) {
 	def cloudUrl = "ps.perfectomobile.com"
 
 	 	try { 
-		def stepStart = httpRequest url: "https://"+ cloudUrl + "/services/executions/" + executionID + "?operation=command&user=" + username + "&password=" + password + "&command=test&subcommand=step&param.name=\" + stepStartName + "\" 
+		def stepStart = httpRequest url: "https://"+ cloudUrl + "/services/executions/" + executionID + "?operation=command&user=" + username + "&password=" + password + "&command=test&subcommand=step&param.name=/" + stepStartName + "/"
 		println stepStart	
 	} catch (all) {  
-		echo 'Failed to Step Start....Catch' 
+		echo 'Failed to Step Start....Catch'  
 		println all 
 	} 
 }
