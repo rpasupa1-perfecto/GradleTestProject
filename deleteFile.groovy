3717c0dc6598ac694937ec624a67c638e7f75a67
John Doe <john@sample.com>
2018-09-11 16:21:49 -0500

    d

--- deleteFile.groovy
+++ deleteFile.groovy
@@ -56,7 +56,7 @@ def deleteFileFromRepository() {
 	def password = "Perfecto123"
 	def cloudUrl = "ebay.perfectomobile.com"
 	def media = "PUBLIC"
-	def mediaFolder = "xcTest/May9"   //eg: Esressop/Folder  or iOS
+	def mediaFolder = "xcTest/MktgTech"   //eg: Esressop/Folder  or iOS
 	def executionID
 	def responseFileData = []
 
